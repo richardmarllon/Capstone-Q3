f93d4b9202f00f06b9f5bf68271ff6ff5b7d0010
diego <kiritokirigayakazuto1@gmail.com>
2021-07-09 11:32:51 -0300

    feat: created model UserLocator, with serialized and hash cpf

--- app/configs/migrations.py
+++ app/configs/migrations.py
@@ -3,5 +3,8 @@ from flask_migrate import Migrate
 
 
 def init_app(app: Flask) -> None:
+
     Migrate(app, app.db, compare_type=True)
 
+    from app.models.user_locator_model import UserLocatorModel
+    from app.models.car_model import CarModel
