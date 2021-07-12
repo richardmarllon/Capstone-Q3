7dd7201655c2635e4909a1953490f1808646f26f
Graziela <grazi.m.kranz@gmail.com>
2021-07-12 16:28:03 -0300

    fix: carmodel and services - alterate to thunk_volume

--- app/services/car_services.py
+++ app/services/car_services.py
@@ -2,7 +2,7 @@ from app.models.car_model import CarModel
 from app.services.helpers import check_incorrect_keys, add_in_db, format_car_plate
 
 def post_car_by_data(data) -> tuple:
-    required_keys = ["year", "car_plate", "model", "trunk_volume", "insurer", "insurer_number", "review_date", "withdrawal_place", "city", "state", "user_id"]
+    required_keys = ["year", "car_plate", "model", "thunk_volume", "insurer", "insurer_number", "review_date", "withdrawal_place", "city", "state", "user_id"]
     check_incorrect_keys(data, required_keys)
 
     car_plate_to_format = format_car_plate(data)
