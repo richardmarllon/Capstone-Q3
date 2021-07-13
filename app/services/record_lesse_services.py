41ca56d43d7ff9735b2f5d5b04bd3a5f67f14d2a
Danilo Kvet Barbosa <danilo.kvet@gmail.com>
2021-07-12 22:38:15 -0300

    feat: adicionado os views para a record_lesse ( CRUD )

---
+++ app/services/record_lesse_services.py
@@ -0,0 +1,40 @@
+from app.models.record_lesse_model import RecordLesseModel
+from app.services.helpers import add_in_db, check_incorrect_keys, check_missing_keys, delete_in_db, commit_current_session
+from http import HTTPStatus
+
+def post_record_lesse_by_data(data) -> tuple:
+    required_keys = ["comment", "lesse_id", "car_id"]
+    check_incorrect_keys(data, required_keys)
+    check_missing_keys(data, required_keys)
+
+    register_lesse = RecordLesseModel(**data)
+    add_in_db(register_lesse)
+    response = register_lesse
+
+    return response, HTTPStatus.CREATED
+
+def search_record_lesse_by_id(id: int) -> tuple:
+    search_result = RecordLesseModel.query.filter_by.get(id)
+
+    return search_result, HTTPStatus.OK
+
+
+def delete_record_lesse_by_id(id: int):
+    register_lesse_to_delete = RecordLesseModel.query.filter_by.get(id)
+
+    if not register_lesse_to_delete:
+        return {"message": f'ID number {id} does not exists.'}, HTTPStatus.NOT_FOUND
+    
+    delete_in_db(register_lesse_to_delete)
+    
+    return "", HTTPStatus.NO_CONTENT
+
+def update_user_less_by_id(id: int, data: dict):
+    user_to_update = RecordLesseModel.query.get(id)
+
+    for key, value in data.items():
+        setattr(user_to_update, key, value)
+
+    add_in_db(user_to_update)
+
+    return user_to_update, HTTPStatus.OK
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -4,8 +4,10 @@ def init_app(app: Flask):
     from app.views.user_locator_view import bp as bp_locator
     from app.views.car_view import bp as bp_car
     from app.views.user_lesse_view import bp as bp_lesse
+    from app.views.record_lesse_view import bp as bp_rec_lesse
 
     app.register_blueprint(bp_locator)
+    app.register_blueprint(bp_rec_lesse)
     app.register_blueprint(bp_car)
     app.register_blueprint(bp_lesse)
 
