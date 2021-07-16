524ace8f5f0742d354c045781d699f5ad5e1f653
Murillo  Augusto <murilo@nvgo.com.br>
2021-07-16 09:43:38 -0300

    feat: create view record locator

---
+++ app/views/record_locator_view.py
@@ -0,0 +1,50 @@
+from flask_jwt_extended.utils import get_jwt_identity
+from flask_jwt_extended.view_decorators import jwt_required
+from app.exc.incorrect_keys_error import IncorrectKeysError
+from app.exc.missing_keys_error import MissingKeys
+from flask import Blueprint, jsonify, request, json
+from http import HTTPStatus
+from app.services.record_locator_services import to_asses_locator, get_avaliation_locator, update_record_locator
+
+bp = Blueprint("rec-locator", __name__, url_prefix="/record")
+
+@bp.post("/locator/assess")
+@jwt_required
+def send_note_record_locator():
+    try:
+        data = request.get_json()
+
+        resp = to_asses_locator(data)
+
+        return jsonify(resp)
+
+    except MissingKeys as error:
+        return error.message, HTTPStatus.BAD_REQUEST
+        
+    except IncorrectKeysError as error:
+        return error.message, HTTPStatus.BAD_REQUEST
+
+    except PermissionError as error:
+        return error, HTTPStatus.UNAUTHORIZED
+
+@bp.get("/locator/<int:user_id>")
+@jwt_required
+def avaliations_locator(user_id: int):
+    
+    user_locator = get_avaliation_locator(user_id)
+
+    if not user_locator:
+        return {"message" : "not found locator"}, HTTPStatus.NOT_FOUND
+
+    return jsonify(user_locator), HTTPStatus.OK
+
+
+@bp.patch("/locator/update/<int:avaliation_id>")
+@jwt_required
+def update_avaliation(avaliation_id):
+
+    data = request.get_json()
+
+    record_locator = update_record_locator(data, avaliation_id)
+
+    return jsonify(record_locator)
