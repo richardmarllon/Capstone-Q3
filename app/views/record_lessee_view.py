c14f2689a77035a3486d6a9783abe02a098e2606
diego <kiritokirigayakazuto1@gmail.com>
2021-07-22 01:05:40 -0300

    fix: records-views

--- app/views/record_lessee_view.py
+++ app/views/record_lessee_view.py
@@ -13,15 +13,16 @@ bp = Blueprint("rec-lessee",__name__, url_prefix="/record/lesse")
 
 @bp.post("/register")
 def post_record_lessee_register():
-    data = request.get_json()
-
     try:
+        data = request.get_json()
+
+   
         response = post_record_lessee_by_data(data)
-        return jsonify(response)
+        return jsonify(response), HTTPStatus.CREATED
     except IncorrectKeysError as err:
-        return err.message
+        return err.message, HTTPStatus.BAD_REQUEST
     except MissingKeys as err:
-        return err.message
+        return err.message, HTTPStatus.BAD_REQUEST
     except IntegrityError as err:
         response = {"message": str(err.__dict__['orig']) }
         return response, HTTPStatus.BAD_REQUEST
@@ -30,13 +31,14 @@ def post_record_lessee_register():
 @bp.patch("/update/<int:user_id>")
 @jwt_required()
 def patch_record_lessee_update(user_id: int):
-    data = request.get_json()
-
     try:
+        data = request.get_json()
         response = update_record_lessee_by_id(user_id, data)
-        return jsonify(response)
+        return jsonify(response), HTTPStatus.OK
     except NotPermission as err:
         return err.message, HTTPStatus.UNAUTHORIZED
+    except NotFound as err:
+        return err.message, HTTPStatus.NOT_FOUND
 
 
 @bp.delete("/delete/<int:user_id>")
@@ -44,9 +46,11 @@ def patch_record_lessee_update(user_id: int):
 def del_record_lessee_delete(user_id: int):
 
     try:
-        return delete_record_lessee_by_id(user_id)
+        return delete_record_lessee_by_id(user_id), HTTPStatus.NO_CONTENT
     except NotPermission as err:
         return err.message, HTTPStatus.UNAUTHORIZED
+    except NotFound as err:
+        return err.message, HTTPStatus.NOT_FOUND
     
 
 
@@ -54,7 +58,7 @@ def del_record_lessee_delete(user_id: int):
 def get_record_lessee(id: int):
     try:
         response = search_record_lessee_by_id(id)
-        return jsonify(response)
+        return jsonify(response), HTTPStatus.OK
     
     except IncorrectKeysError as err:
         return err.message
